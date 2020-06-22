ba485fc5ab4c1669bc9a9defc04aecce3c4ab40c
Jijo <jijogeorgeab@gmail.com>
2020-06-22 09:36:50 +0530

    Added search option

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,7 +7,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset') #hello-viewset is the url for accessing viewSet fuunctions
-
+router.register('profile',views.UserProfileViewSet)
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
     path('', include(router.urls)), #Automatically create url patterns for all functions defined in ViewSet class in view.py
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -3,6 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
 from rest_framework import viewsets
+from profiles_api import models
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
+from rest_framework import filters #For searching objects
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -48,6 +52,7 @@ class HelloApiView(APIView):
         """Delete an object"""
 
         return Response({'method': 'DELETE'})
+
 
 class HelloViewSet(viewsets.ViewSet):
     """Test API ViewSet"""
@@ -96,3 +101,14 @@ class HelloViewSet(viewsets.ViewSet):
         """Handle removing an object"""
 
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    #For searching the objects
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',) #allows to search by name and email fields
